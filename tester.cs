02bdaddc3fde7669ebb3a2c081c6a525ceddbfba
Nelff <65121492+Nelff@users.noreply.github.com>
2020-05-10 20:08:16 +0500

    Add files via upload

---
+++ tester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloApp
+{
+    public class tester
+    {
+       
+        public int Id_tester{ get; set; }
+        public string FIO_tester { get; set; }
+      
+    }
+}
+
