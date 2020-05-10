98d46b6456dbf770fb87077538d2222a0a73d829
Nelff <65121492+Nelff@users.noreply.github.com>
2020-05-10 20:06:42 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace HelloApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // создаем два объекта User
+                tester user1 = new tester { FIO_tester = ""};
+                tester user2 = new tester { FIO_tester = "Alice"};
+             
+                // добавляем их в бд
+                db.tester.Add(user1);
+                db.tester.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var users = db.tester.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (tester u in users)
+                {
+                    Console.WriteLine($"{u.Id_tester}.{u.FIO_tester}");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
